17a59c62f9b1b3e9506f9b8bec6516a83c271e23
daniiflexx <99187639+daniiflexx@users.noreply.github.com>
2022-10-28 18:45:27 +0200

    quicksort p2

---
+++ p2/sorting.c
@@ -0,0 +1,221 @@
+/**
+ *
+ * Descripcion: Implementation of sorting functions
+ *
+ * Fichero: sorting.c
+ * Autor: Carlos Aguirre
+ * Version: 1.0
+ * Fecha: 16-09-2019
+ *
+ */
+
+
+#include "sorting.h"
+#include <stdio.h>
+/***************************************************/
+/* Function: SelectSort Date: 21/10/2022           */
+/* Authors: Daniel Birsan y Daniel Cruz            */
+/*                                                 */
+/* Sorts increasingly                              */
+/* a given array                                   */
+/*                                                 */
+/* Input: An array and its first and last indexes  */
+/* int *array: the array                           */
+/* int ip: first index of the array                */
+/* int iu: last index of the array                 */
+/* Output: Number of iterated OB                   */
+/* int: OB times                                   */
+/***************************************************/
+int SelectSort(int* array, int ip, int iu)
+{
+  int i, minim, aux, x;
+
+  if (!array || ip < 0 || ip > iu)
+    return ERR;
+  
+  minim = 0;
+  for(i = ip, x = 0; i < iu; i++)
+  {
+    x += iu - i;
+    minim  = min(array, i, iu);
+    if (minim == ERR)
+      return ERR;
+    aux = array[i];
+    array[i] = array[minim];
+    array[minim] = aux;
+  }
+  return (x);
+}
+
+/***************************************************/
+/* Function: SelectSort Date: 21/10/2022           */
+/* Authors: Daniel Birsan y Daniel Cruz            */
+/*                                                 */
+/* Sorts decreasingly                              */
+/* a given array                                   */
+/*                                                 */
+/* Input: An array and its first and last indexes  */
+/* int *array: the array                           */
+/* int ip: first index of the array                */
+/* int iu: last index of the array                 */
+/* Output: Number of iterated OB                   */
+/* int: OB times                                   */
+/***************************************************/
+int SelectSortInv(int *array, int ip, int iu)
+{
+  int i, maxim, aux, x;
+  
+  if (!array || ip < 0 || ip > iu)
+    return ERR;
+
+  for (i = ip, x = 0; i < iu; i++)
+  {
+    x += iu - i;
+    
+    maxim = max(array, i, iu);
+    if (maxim == ERR)
+      return ERR;
+    
+    aux = array[i];
+    array[i] = array[maxim];
+    array[maxim] = aux;
+  }
+
+  return x;
+}
+
+/***************************************************/
+/* Function: min Date: 21/10/2022                  */
+/* Authors: Daniel Birsan y Daniel Cruz            */
+/*                                                 */
+/* Returns the minimum from                        */
+/* a given array                                   */
+/*                                                 */
+/* Input: An array and its first and last indexes  */
+/* int *array: the array                           */
+/* int ip: first index of the array                */
+/* int iu: last index of the array                 */
+/* Output: minimum of the array                    */
+/* int: the minimum                                */
+/***************************************************/
+int min(int* array, int ip, int iu)
+{
+  int i = 0;
+  int minim = 0;
+
+  if (!array || ip < 0 || ip > iu)
+    return ERR;
+
+  minim = ip;
+  for(i = ip + 1; i <= iu; i++)
+  {
+    if (array[i] < array[minim])
+      minim = i;
+  }
+  return minim;
+}
+
+/***************************************************/
+/* Function: max Date: 21/10/2022                  */
+/* Authors: Daniel Birsan y Daniel Cruz            */
+/*                                                 */
+/* Returns the maximum from                        */
+/* a given array                                   */
+/*                                                 */
+/* Input: An array and its first and last indexes  */
+/* int *array: the array                           */
+/* int ip: first index of the array                */
+/* int iu: last index of the array                 */
+/* Output: maximum of the array                    */
+/* int: the maximum                                */
+/***************************************************/
+int max(int *array, int ip, int iu)
+{
+  int i;
+  int minim;
+
+  if (!array || ip < 0 || ip > iu)
+    return ERR;
+  
+  minim = ip;
+  
+  for (i = ip + 1; i <= iu; i++)
+  {
+    if (array[i] > array[minim])
+      minim = i;
+  }
+  
+  return minim;
+}
+
+int quicksort(int* tabla, int ip, int iu)
+{
+  int n;
+  int m;
+
+  n = 0;
+  if (ip > iu)
+    return ERR;
+
+  if (ip == iu)
+    return n;
+  else 
+  {
+    n += partition(tabla, ip, iu, &m);
+    if (ip < m - 1)
+    n += quicksort(tabla, ip, m - 1);
+    if(m + 1 < iu)
+    n += quicksort(tabla, m + 1, iu);
+  }
+  return n;
+}
+
+int partition(int* tabla, int ip, int iu,int *pos)
+{
+  int i, n, k, aux;
+  
+  n = 0;
+  n = median(tabla, ip, iu, pos);
+  k = tabla[*pos];
+  tabla[*pos] = tabla[ip];
+  tabla[ip] = k;
+  *pos = ip;
+  for (i = ip+1; i <= iu; i++) 
+  {
+    if (tabla[i] < k)
+    {
+      (*pos)++;
+      aux = tabla[i];
+      tabla[i] = tabla[*pos];
+      tabla[*pos] = aux;
+    }
+    aux = tabla[*pos];
+    n++;
+  }
+  tabla[*pos] = tabla[ip];
+  tabla[ip] = aux;
+  return n;
+}
+
+int median(int *tabla, int ip, int iu,int *pos)
+{
+  *pos = ip;
+  return (0);
+}
+
+int median_avg(int *tabla, int ip, int iu, int *pos) {
+  *pos = (ip + iu)/2;
+  return (0);
+}
+
+int median stat(int *tabla, int ip, int iu, int *pos) {
+  int x;
+
+  median_avg(tabla, ip, iu, *pos);
+  if (tabla[iu] > tabla[*pos] && tabla[ip] > tabla[iu]);
+    *pos = iu;
+  if (tabla[ip] > tabla[*pos] && tabla[iu] > tabla[ip]);
+    *pos = ip;
+
+  
+}
---
+++ p2/times.c
@@ -0,0 +1,149 @@
+/**
+ *
+ * Descripcion: Implementation of time measurement functions
+ *
+ * Fichero: times.c
+ * Autor: Carlos Aguirre Maeso
+ * Version: 1.0
+ * Fecha: 16-09-2019
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "times.h"
+#include "sorting.h"
+#include "permutations.h"
+
+
+/***************************************************/
+/*                                                 */
+/* Function: AverageSortingTime Date: 21/10/2022   */
+/* Authors: Daniel Birsan y Daniel Cruz            */
+/*                                                 */
+/* Input: A sorting method, perms and time DataStr.*/
+/* pfunc_sort: sorting method                      */
+/* int n_perms: number of permutations             */
+/* int N: number of executions                     */
+/* PTIME_AA: Time data structure                   */
+/* Output: OK if everything ran correctly,         */
+/*    otherwise ERR                                */
+/* short: OK, ERR                                  */
+/***************************************************/
+short average_sorting_time(pfunc_sort metodo, 
+                              int n_perms,
+                              int N, 
+                              PTIME_AA ptime)
+{
+  int **perms, **aux;
+  int i, OB = 0, min_OB = 2147483647, max_OB = -2147483648;
+  double av_OB = 0;
+  clock_t begin, end;
+  double time = 0.0;
+
+  if (!metodo || n_perms < 0 || N < 0 || ptime == NULL)
+    return ERR;
+  perms = generate_permutations(n_perms, N);
+  if (!perms)
+    return ERR;
+  aux = perms;
+  for (i = 0; i < n_perms; i++) {
+    begin = clock();
+    if ((OB = metodo(*perms, 0, N - 1) ) == ERR) {
+      for ( i = 0; i < n_perms; i++) {
+        free(aux[i]);
+      }
+      free(aux);
+      return ERR;
+    }
+    end = clock();
+    av_OB += OB;
+    if (OB <= min_OB)
+      min_OB = OB;
+    if (OB >= max_OB)
+      max_OB = OB;
+    time += (double) (end - begin);
+    perms++;
+  }
+  
+  av_OB /= n_perms;
+  time /= n_perms;
+  ptime->min_ob = min_OB;
+  ptime->max_ob = max_OB;
+  ptime->average_ob = av_OB;
+  ptime->time = time / CLOCKS_PER_SEC;
+  ptime->n_elems = n_perms;
+  ptime->N = N;
+  
+  for ( i = 0; i < n_perms; i++)
+  {
+    free(aux[i]);
+  }
+  free(aux);
+  return OK;
+}
+
+/***************************************************/
+/*                                                 */
+/* Function: GenerateSortingTimes Date: 21/10/2022 */
+/* Authors: Daniel Birsan y Daniel Cruz            */
+/*                                                 */
+/* Input: A sorting method, OBs data and a file    */
+/* pfunc_sort: sorting method                      */
+/* int n_perms: number of permutations             */
+/* int num_min: minimun number of OB               */
+/* int num_max: maximum number of OB               */
+/* char* file: file where data will be written     */
+/* int incr: increase                              */
+/* Output: OK if everything ran correctly,         */
+/*    otherwise ERR                                */
+/* short: OK, ERR                                  */
+/***************************************************/
+short generate_sorting_times(pfunc_sort method, char* file, 
+                                int num_min, int num_max, 
+                                int incr, int n_perms)
+{
+  int i;
+  TIME_AA times;
+  FILE *f;
+
+  f = fopen(file, "w");
+  if (!f)
+    return ERR;
+  fprintf(f, "EXERCISE 5\n");
+  fclose(f);
+  for ( i = num_min; i <= num_max; i+=incr)
+  {
+    if (average_sorting_time(method, n_perms, i, &times) == ERR)
+      return ERR;
+    if (save_time_table(file, &times, i) == ERR)
+      return ERR;
+  }
+  return OK;
+}
+
+/***************************************************/
+/*                                                 */
+/* Function: SaveTimeTable Date: 21/10/2022        */
+/* Authors: Daniel Birsan y Daniel Cruz            */
+/*                                                 */
+/* Input: file, number of inputs and time DataStr. */
+/* int n_times: number of times to write           */
+/* PTIME_AA: Time data structure                   */
+/* char* file: file where data is written          */
+/* Output: OK if everything ran correctly,         */
+/*    otherwise ERR                                */
+/* short: OK, ERR                                  */
+/***************************************************/
+short save_time_table(char* file, PTIME_AA ptime, int n_times)
+{
+  FILE *f;
+
+  f = fopen(file, "a+");
+  if (!f)
+    return ERR;
+  fprintf(f, "SIZE: %d:\nAverage time: %f\nAverage OB: %f\nMax_ob: %d\nMin_ob:%d\n\n", n_times, ptime->time, ptime->average_ob, ptime->max_ob, ptime->min_ob);
+  fclose(f);
+  return OK;
+}
