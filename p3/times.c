ff158423dc301f706ec33397ffaaa33fcd26812a
danii1107 <99350151+danii1107@users.noreply.github.com>
2022-12-09 17:37:58 +0100

    Update times.c

--- p3/times.c
+++ p3/times.c
@@ -14,13 +14,15 @@
 #include <time.h>
 #include "times.h"
 #include "sorting.h"
+#include "search.h"
 #include "permutations.h"
 
 
 short average_search_time(pfunc_search metodo, pfunc_key_generator generator, int order, int N, int n_times, PTIME_AA ptime) 
 {
   PDICT dic;
-  int *perm, *keys, i, pos = 0, ob = 0, maxob = -(__INT_MAX__ + 1), minob = __INT_MAX__;
+  int *perm, *keys, i, pos = 0, ob = 0;
+  int maxob = -(__INT_MAX__), minob = __INT_MAX__;
   clock_t start, end;
   float avg = 0, avgob = 0;
 
@@ -30,7 +32,7 @@ short average_search_time(pfunc_search metodo, pfunc_key_generator generator, in
   
   perm = generate_perm(N);
   if (!perm) {
-    free_dictionary(dic)
+    free_dictionary(dic);
     return ERR;
   }
 
@@ -52,7 +54,7 @@ short average_search_time(pfunc_search metodo, pfunc_key_generator generator, in
   ptime->n_elems = N*n_times;
   for (i = 0; i < ptime->n_elems; i++) {
     start = clock();
-    if ((ob = metodo(dic, 0, N-1, keys[i], &pos)) == ERR) {
+    if ((ob = metodo(dic->table, 0, N-1, keys[i], &pos)) == ERR) {
       free_dictionary(dic);
       free(perm);
       free(keys);
@@ -79,6 +81,21 @@ short average_search_time(pfunc_search metodo, pfunc_key_generator generator, in
 
 short generate_search_times(pfunc_search method, pfunc_key_generator generator, int order, char* file, int num_min, int num_max, int incr, int n_times)
 {
+  int i;
+  TIME_AA times;
+  FILE *f;
+
+  f = fopen(file, "w");
+  if (!f)
+    return ERR;
+  fclose(f);
+  for ( i = num_min; i <= num_max; i+=incr)
+  {
+    if (average_search_time(method, generator, order, i, n_times ,&times) == ERR)
+      return ERR;
+    if (save_time_table(file, &times, i) == ERR)
+      return ERR;
+  }
   return OK;
 }
 
